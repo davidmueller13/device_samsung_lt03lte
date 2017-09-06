85b3229aa1974101a9b3e941f8e2a99b6ad2607a
Adrian DC <radian.dc@gmail.com>
2017-09-05 20:03:00 -0400

    lt03lte: camera: Use wrapper parameters instead of CameraParameters
     * Get inline with the lack of CameraParametersExtra.h
        support in AOSP, avoid using device header to inject
        additional parameters, especially with the introduction
        of Android.bp compilation for libcamera_client
        which ignores the device headers override
    
    Change-Id: I754116f55ed32d54d1b1ad44407993137a07fb69
    
    # Conflicts:
    #	camera/CameraWrapper.cpp
    #	include/camera/CameraParametersExtra.h

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -39,6 +39,12 @@
 
 using namespace android;
 
+static const char KEY_DIS[] = "dis";
+static const char DIS_DISABLE[] = "disable";
+static const char KEY_ZSL[] = "zsl";
+static const char ZSL_ON[] = "on";
+static const char ZSL_OFF[] = "off";
+
 static Mutex gCameraWrapperLock;
 static camera_module_t *gVendorModule = 0;
 
@@ -194,10 +200,10 @@ static char *camera_fixup_setparams(int id, const char *settings)
 
     if (!isTorch && !wasTorch) {
         if (isVideo) {
-            params.set(CameraParameters::KEY_DIS, CameraParameters::DIS_DISABLE);
-            params.set(CameraParameters::KEY_ZSL, CameraParameters::ZSL_OFF);
+            params.set(KEY_DIS, DIS_DISABLE);
+            params.set(KEY_ZSL, ZSL_OFF);
         } else {
-            params.set(CameraParameters::KEY_ZSL, CameraParameters::ZSL_ON);
+            params.set(KEY_ZSL, ZSL_ON);
         }
     }
 
