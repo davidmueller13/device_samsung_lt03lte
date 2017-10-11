63d8e2b5b512d051ae5e1be9aab7d1cd7f7a5326
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-11 19:42:26 +0300

    lt03lte: Add vibrator HAL impl
    Change-Id: I2fcd62b1dd6cbe292d89cf97085c9549bf69ba33
    
    # Conflicts:
    #	device.mk

--- device.mk
+++ device.mk
@@ -147,6 +147,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
 # WiFi
 PRODUCT_PACKAGES += \
     libnetcmdiface \
