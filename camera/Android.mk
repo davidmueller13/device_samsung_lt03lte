0851025f49ff3357cb2eda1d2d393284756c6568
Rashed Abdel-Tawab <rashed@linux.com>
2017-11-01 18:21:09 -0700

    lt03lte: CameraWrapper: Include nativebase headers
     * This is required as of android-8.0.0_r23
    
    Change-Id: Ic76cfff3a5bfe46c0ea6974e1ffce9fb7ed069c6
    
    # Conflicts:
    #	camera/Android.mk

--- camera/Android.mk
+++ camera/Android.mk
@@ -19,9 +19,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES += \
-    system/media/camera/include
-
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
@@ -30,6 +27,11 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils libcutils libdl \
     android.hidl.token@1.0-utils \
     android.hardware.graphics.bufferqueue@1.0
+
+LOCAL_C_INCLUDES := \
+    system/media/camera/include
+
+LOCAL_HEADER_LIBRARIES := libnativebase_headers
 
 LOCAL_MODULE := camera.msm8974
 LOCAL_MODULE_RELATIVE_PATH := hw
