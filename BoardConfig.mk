f9a36ea5c3759c4d42349d520712df577eac3720
Kevin F. Haggerty <haggertk@lineageos.org>
2018-06-14 05:51:42 -0600

    lt03lte: Rework shims
    * Recent binder changes caused our approach of shimming libcutils to
      re-add the non-inlined android_atomic* methods to start getting
      slapped, at least with NDK apps. This looks oddly similar to the
      heads-up we received in 7.0, so no clue why it let us do it in 8.*
      prior to now.
    * See: https://developer.android.com/about/versions/nougat/android-7.0-changes#ndk
    * We did this because all of the libsec-ril* blobs and libuiblur
      needed it and doing each and every libsec-ril seemed fragile and
      fraught with the potential for error, but "fragile and fraught with
      the potential for error" is better than "phone breaking". And blur
      doesn't seem to ever be coming back.
    * We'll leave the "standard" libsec-ril.so here and let the
      individual devices handle anything specific if they have variant-
      specific blobs
    
    Change-Id: Ib11048e4924f34ade20f44b707f0106e139f2f82
    
    # Conflicts:
    #	BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,7 +87,9 @@ DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
 # Legacy BLOB Support
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
-TARGET_LD_SHIM_LIBS := /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so:/system/vendor/lib/hw/camera.vendor.msm8974.so|libshim_camera.so
+TARGET_LD_SHIM_LIBS += \ 
+	/system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
+	/system/vendor/lib/hw/camera.vendor.msm8974.so|libshim_camera.so
 
 # Pre-Optimize DEX
 WITH_DEXPREOPT := true
